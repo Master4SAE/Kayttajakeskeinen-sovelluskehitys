dc90e79e977a276718395c7d2def70619977c593
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-11-21 00:01:21 +0200

    hello

--- src/app.js
+++ src/app.js
@@ -1,71 +1,30 @@
 import express from 'express'
-import fs from 'fs'
+import path from 'path';
+import bodyParser from 'body-parser';
+import apiRoutes from './api';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+// Set Pug as the view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.set('views', 'src/views');
+// Serve static files from the 'media' folder
+app.use('/media', express.static(path.join(__dirname, 'media')));
 
+// Middleware to parse JSON in request body
+app.use(bodyParser.json());
 
+// Use API routes
+app.use('/api', apiRoutes);
+
+// Render HTML page using Pug
 app.get('/', (req, res) => {
-    res.render('index', { title: 'My Pug Page', message: 'Welcome to Pug!' });
-
-});
-//Get all
-app.get('/api/media', (req, res) => {
-    fs.readFile('api/media/allMockData.json', 'utf8', (err, data) => {
-      if (err) {
-        res.status(500).send('Error reading the JSON file');
-      } else {
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
-      }
-    });
-  });
-
-app.get('/api/media/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    console.log(id);
-    
-    fs.readFile('api/media/allMockData.json', 'utf8', (err, data) => {
-        if (err) {
-          res.status(500).send('Error reading the JSON file');
-        } else {
-            const jsonData = JSON.parse(data);
-
-            let matchFound = false;
-            jsonData.forEach(element => {
-                if (element.media_id === id){
-                    res.send(element);
-                    matchFound = true;
-                }
-            });
-            if (!matchFound) {
-                res.status(404).send('No matching data found');
-              }
-          
-        }
-      });
-
+    res.render('index', { title: 'Media Sharing API' });
 });
 
-app.post('/api/media/submit', (req, res) => {
-  // Access data from the request body
-  const formData = req.body;
-
-  res.render('submitForm');
-
-  // Process the data (you can replace this with your own logic)
-  console.log('Form data received:', formData);
-
-  // Send a response back to the client
-  res.send('Form submitted successfully!');
-});
-
-
-
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on http://localhost:${port}`);
+});
