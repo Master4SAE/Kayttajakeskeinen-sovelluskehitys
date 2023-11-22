7ae1b8903ab6575919753e138631b6338b8a9dcf
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-11-22 23:29:26 +0200

    i am happy i did it finally

--- src/app.js
+++ src/app.js
@@ -6,20 +6,6 @@ import apiRoutes from './api';
 const app = express();
 const port = 3000;
 
-// Set Pug as the view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// Serve static files from the 'media' folder
-app.use('/media', express.static(path.join(__dirname, 'media')));
-
-// Middleware to parse JSON in request body
-app.use(bodyParser.json());
-
-// Use API routes
-app.use('/api', apiRoutes);
-
-// Render HTML page using Pug
 app.get('/', (req, res) => {
     res.render('index', { title: 'Media Sharing API' });
 });
